1532b480b5d735bd173ea481c0a0be5936477d9b
Alex <alexstlopes@gmail.com>
2017-10-17 23:12:19 -0300

    Veirificar erro

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    #url(r'^$', views.index, name='index'),
     url(r'^books/$', views.BookListView.as_view(), name='books'),
     url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
     url(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
@@ -13,4 +13,12 @@ urlpatterns = [
 
 urlpatterns += [
     url(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+]
+
+urlpatterns += [
+    url(r'^book/(?P<pk>[-\w]+)/renew/$', views.renew_book_librarian, name='renew-book-librarian'),
+]
+
+urlpatterns += [
+    url(r'booksborrowed/$', views.AllBorrowedBooksListView.as_view(), name='all-borrowed'),
 ]
